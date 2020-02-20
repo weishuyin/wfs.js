a675f4ca27a7c7ec2e8f1cb2733d73f42b786a58
shuyin.wsy <shuyin.wsy@alibaba-inc.com>
2020-02-20 11:42:46 +0800

    fix dyld: malformed mach-o image: segment __DWARF has vmsize < filesize

--- server/build_lite.sh
+++ server/build_lite.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  
-go build -o wfs_server wfs_server_lite.go wfs_websockets_hub.go
+go build -ldflags "-w" -o wfs_server wfs_server_lite.go wfs_websockets_hub.go
 mv ./wfs_server ../demo/
